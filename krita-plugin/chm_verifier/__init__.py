4cac32a51e08ff5c5d3a5ae4e58687b0c6d82303
dabhunt <50185040+dabhunt@users.noreply.github.com>
2025-12-28 17:27:45 -0800

    Fix __init__.py import - add missing Krita import
    - Added 'from krita import Krita' to __init__.py
    - Fixes NameError when plugin tries to load
    - Plugin should now appear in Krita's Plugin Manager

--- krita-plugin/chm_verifier/__init__.py
+++ krita-plugin/chm_verifier/__init__.py
@@ -4,8 +4,9 @@ Certified Human-Made (CHM) Verifier - Krita Plugin
 This plugin captures art creation events to generate cryptographic proofs of human authorship.
 """
 
+from krita import Krita
 from .chm_extension import CHMExtension
 
-# Kritaplugin's metadata
+# Krita plugin's metadata
 Krita.instance().addExtension(CHMExtension(Krita.instance()))
 
